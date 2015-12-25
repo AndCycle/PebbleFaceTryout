f3f070670b918e35889c8c707fb14fc7c290ad49
AndCycle <andcycle-github@andcycle.idv.tw>
2015-12-25 19:21:02 +0800

    submit

--- src/setting.js
+++ src/setting.js
@@ -2,3 +2,7 @@ Pebble.addEventListener('showConfiguration', function(e) {
   // Show config page
   Pebble.openURL('https://my-website.com/config-page.html');
 });
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  console.log('Configuration window returned: ' + e.response);
+});
